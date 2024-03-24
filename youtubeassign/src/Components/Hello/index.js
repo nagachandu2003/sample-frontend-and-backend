ee0d05f7492fda8403fa8115b964080cd6879570
nagachandu2003 <nagachanduthatha@gmail.com>
2024-03-24 19:52:29 +0530

    added serverver

--- youtubeassign/src/Components/Hello/index.js
+++ youtubeassign/src/Components/Hello/index.js
@@ -9,7 +9,7 @@ class Hello extends Component {
     }
 
     getData = async () => {
-        const response = await fetch("http://localhost:8085/users");
+        const response = await fetch("https://sample-frontend-and-backend.vercel.app/users");
         const data = await response.json()
         this.setState({itemsList:data})
     };
@@ -27,7 +27,7 @@ class Hello extends Component {
             },
             body: JSON.stringify(data) // stringify the JSON data
         };
-        const response = await fetch("http://localhost:8085/users", options);
+        const response = await fetch("https://sample-frontend-and-backend.vercel.app/users", options);
         if(response.ok)
         console.log("Password Updated Successfully");
         else
@@ -47,7 +47,7 @@ class Hello extends Component {
             },
             body: JSON.stringify(data) // stringify the JSON data
         };
-        const response = await fetch("http://localhost:8085/users", options);
+        const response = await fetch("https://sample-frontend-and-backend.vercel.app/users", options);
         if(response.ok)
         console.log("User inserted Successfully");
         else
@@ -65,7 +65,7 @@ class Hello extends Component {
         const options = {
             method: "DELETE"
         };
-        const response = await fetch(`http://localhost:8085/users/${userinput}`, options);
+        const response = await fetch(`https://sample-frontend-and-backend.vercel.app/users/${userinput}`, options);
         if (response.ok) {
             console.log("User deleted successfully");
         } else {
